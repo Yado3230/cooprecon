47600d16e3f47ca645575d15ed65852322ee628f
Yado3230 <yaredbest81@gmail.com>
2024-04-08 12:08:53 +0300

    final

--- actions/file-type.acion.ts
+++ actions/file-type.acion.ts
@@ -1,8 +1,10 @@
 import { FileType, FileTypeRequest } from "@/types/types";
 
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+
 export const getAllFileTypes = async (): Promise<FileType[]> => {
   try {
-    const res = await fetch(`/api/v1/file-types`);
+    const res = await fetch(`${API_URL}api/v1/file-types`);
     return res.json();
   } catch (error) {
     console.error("Error:", error);
@@ -12,7 +14,7 @@ export const getAllFileTypes = async (): Promise<FileType[]> => {
 
 export const AddFileType = async (data: FileTypeRequest): Promise<FileType> => {
   try {
-    const response = await fetch(`/api/v1/file-types`, {
+    const response = await fetch(`${API_URL}api/v1/file-types`, {
       method: "POST",
       body: JSON.stringify(data),
     });
@@ -31,7 +33,7 @@ export const AddFileType = async (data: FileTypeRequest): Promise<FileType> => {
 
 export const EditFileType = async (data: FileType[]): Promise<any> => {
   try {
-    const response = await fetch(`/api/v1/file-types`, {
+    const response = await fetch(`${API_URL}api/v1/file-types`, {
       method: "PUT",
       body: JSON.stringify(data),
     });
